fb496be34d7dd4692f0464c39b156f748533e7f2
Denis Khamzin <96993113+DenisKhamzin@users.noreply.github.com>
2026-05-26 05:41:36 +0300

    TestAddGetDelete is ready

--- parcel_test.go
+++ parcel_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,91 +32,108 @@ func getTestParcel() Parcel {
 // TestAddGetDelete проверяет добавление, получение и удаление посылки
 func TestAddGetDelete(t *testing.T) {
 	// prepare
-	db, err := // настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db") // настройте подключение к БД
+	require.NoError(t, err, "Require: Driver")
+	defer db.Close()
+
 	store := NewParcelStore(db)
 	parcel := getTestParcel()
 
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+	number, err := store.Add(parcel)
+	require.NoError(t, err, "Require: Add")
+	assert.NotNil(t, number, "Assert: Add")
 
 	// get
 	// получите только что добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что значения всех полей в полученном объекте совпадают со значениями полей в переменной parcel
+	getParcel, err := store.Get(number)
+	require.NoError(t, err, "Require: Get")
+	assert.Equal(t, getParcel.Number, number, "Assert: Get, Number")
+	assert.Equal(t, getParcel.Client, parcel.Client, "Assert: Get, Client")
+	assert.Equal(t, getParcel.Address, parcel.Address, "Assert: Get, Address")
+	assert.Equal(t, getParcel.Status, parcel.Status, "Assert: Get, Status")
+	assert.Equal(t, getParcel.CreatedAt, parcel.CreatedAt, "Assert: Get, Created At")
 
 	// delete
 	// удалите добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что посылку больше нельзя получить из БД
+	err = store.Delete(number)
+	require.NoError(t, err, "Require: Delete")
+	_, err = store.Get(number)
+	require.Error(t, err, "Assert: Delete (Get)")
 }
 
 // TestSetAddress проверяет обновление адреса
-func TestSetAddress(t *testing.T) {
-	// prepare
-	db, err := // настройте подключение к БД
-
-	// add
-	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
-
-	// set address
-	// обновите адрес, убедитесь в отсутствии ошибки
-	newAddress := "new test address"
-
-	// check
-	// получите добавленную посылку и убедитесь, что адрес обновился
-}
-
+//func TestSetAddress(t *testing.T) {
+//	// prepare
+//	db, err := // настройте подключение к БД
+//
+//	// add
+//	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+//
+//	// set address
+//	// обновите адрес, убедитесь в отсутствии ошибки
+//	newAddress := "new test address"
+//
+//	// check
+//	// получите добавленную посылку и убедитесь, что адрес обновился
+//}
+//
 // TestSetStatus проверяет обновление статуса
-func TestSetStatus(t *testing.T) {
-	// prepare
-	db, err := // настройте подключение к БД
+//func TestSetStatus(t *testing.T) {
+// prepare
+//	db, err := // настройте подключение к БД
 
-	// add
-	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+// add
+// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 
-	// set status
-	// обновите статус, убедитесь в отсутствии ошибки
+// set status
+// обновите статус, убедитесь в отсутствии ошибки
 
-	// check
-	// получите добавленную посылку и убедитесь, что статус обновился
-}
+// check
+// получите добавленную посылку и убедитесь, что статус обновился
+//}
 
 // TestGetByClient проверяет получение посылок по идентификатору клиента
-func TestGetByClient(t *testing.T) {
-	// prepare
-	db, err := // настройте подключение к БД
+//func TestGetByClient(t *testing.T) {
+// prepare
+//	db, err := // настройте подключение к БД
 
-	parcels := []Parcel{
-		getTestParcel(),
-		getTestParcel(),
-		getTestParcel(),
-	}
-	parcelMap := map[int]Parcel{}
+//	parcels := []Parcel{
+//		getTestParcel(),
+//		getTestParcel(),
+//		getTestParcel(),
+//	}
+//	parcelMap := map[int]Parcel{}
 
-	// задаём всем посылкам один и тот же идентификатор клиента
-	client := randRange.Intn(10_000_000)
-	parcels[0].Client = client
-	parcels[1].Client = client
-	parcels[2].Client = client
+// задаём всем посылкам один и тот же идентификатор клиента
+//	client := randRange.Intn(10_000_000)
+//	parcels[0].Client = client
+//	parcels[1].Client = client
+//	parcels[2].Client = client
 
-	// add
-	for i := 0; i < len(parcels); i++ {
-		id, err := // добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+// add
+//	for i := 0; i < len(parcels); i++ {
+//		id, err := // добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 
-		// обновляем идентификатор добавленной у посылки
-		parcels[i].Number = id
+// обновляем идентификатор добавленной у посылки
+//		parcels[i].Number = id
 
-		// сохраняем добавленную посылку в структуру map, чтобы её можно было легко достать по идентификатору посылки
-		parcelMap[id] = parcels[i]
-	}
+// сохраняем добавленную посылку в структуру map, чтобы её можно было легко достать по идентификатору посылки
+//		parcelMap[id] = parcels[i]
+//	}
 
-	// get by client
-	storedParcels, err := // получите список посылок по идентификатору клиента, сохранённого в переменной client
-	// убедитесь в отсутствии ошибки
-	// убедитесь, что количество полученных посылок совпадает с количеством добавленных
+// get by client
+//	storedParcels, err := // получите список посылок по идентификатору клиента, сохранённого в переменной client
+// убедитесь в отсутствии ошибки
+// убедитесь, что количество полученных посылок совпадает с количеством добавленных
 
-	// check
-	for _, parcel := range storedParcels {
-		// в parcelMap лежат добавленные посылки, ключ - идентификатор посылки, значение - сама посылка
-		// убедитесь, что все посылки из storedParcels есть в parcelMap
-		// убедитесь, что значения полей полученных посылок заполнены верно
-	}
-}
+// check
+//	for _, parcel := range storedParcels {
+// в parcelMap лежат добавленные посылки, ключ - идентификатор посылки, значение - сама посылка
+// убедитесь, что все посылки из storedParcels есть в parcelMap
+// убедитесь, что значения полей полученных посылок заполнены верно
+//	}
+//}
